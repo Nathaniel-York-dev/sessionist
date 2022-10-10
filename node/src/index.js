75b22a9b77e156c649433ac2a095cc3f7ccd31cd
Nathaniel-York-dev <nathanielyork.dev@gmail.com>
2022-10-10 11:37:20 -0500

    [master] fi: remove cors

--- node/src/index.js
+++ node/src/index.js
@@ -23,9 +23,7 @@ const apis = {
     food: 'https://www.themealdb.com/api/json/v1/1/',
     covid: 'https://covid19.mathdro.id/api',
 }
-app.use(cors({
-    origin: '*'
-}));
+
 // Session middleware
 app.use(session({
     secret: 'keyboard cat',
